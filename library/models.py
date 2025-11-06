4eeddd4930e8b62a24482aae3cb004ba788226bb
mierkulova-tech <mierkulova.tech@gmail.com>
2025-11-06 14:40:08 +0100

    Update Book model

--- library/models.py
+++ library/models.py
@@ -19,14 +19,30 @@ class Author(models.Model):
         return f"{self.last_name[0]}.{self.first_name}"
 
 class Book(models.Model):
+    GENRE_CHOICES=[
+        ('FICTION','Fiction'), # 1 - в базу, 2 клиенту
+        ('NON-FICTION','Non-Fiction'),
+        ('SCIENCE FICTION','Science Fiction'),
+        ('FANTASY','Fantasy'),
+        ('MYSTERY','Mystery'),
+        ('BIOGRAPHY','Biography'),
+    ]
+
+
     title = models.CharField(max_length=255, verbose_name="Название книги")
-    publication_date = models.DateField(blank=True, null=True, verbose_name="дата публикации")
+    publication_date = models.DateField(blank=True, null=True, verbose_name="Дата публикации")
     author = models.ForeignKey(
         "Author",
         on_delete=models.SET_NULL,
         null=True,
         related_name="books"
     )
+    description = models.TextField(blank=True, null=True, verbose_name="Краткое описание")
+    page_count = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(10_000)],
+        blank=True, null=True, verbose_name="Страницы"
+    )
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES, default='BIOGRAPHY')
 
     def __str__(self):
         return f"{self.title} --{self.author.last_name if self.author else 'NONAME'}"
